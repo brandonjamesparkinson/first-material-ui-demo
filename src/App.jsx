e25a468c05d7e253d0af969ba6abfd91b1dc0c38
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2022-03-29 16:50:25 +0100

    + complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,86 @@
 import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, ButtonGroup, Button, makeStyles } from '@material-ui/core'
+import { PhotoCamera } from '@material-ui/icons'
+import useStyles from './styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const App = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>
-               Hello, World! 
-            </h1>
-        </div>
+        <>
+        <CssBaseline/>
+        <AppBar position="relative">
+            <Toolbar>
+               <PhotoCamera className={classes.icon}/>
+               <Typography variant="h6">
+                    Photo Album
+               </Typography>
+            </Toolbar>
+        </AppBar>
+        <main>
+            <div className={classes.container}>
+                <Container maxWidth="sm">
+                    <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                        Photo Album
+                    </Typography>
+                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                        Welcome
+                    </Typography>
+                    <div className={classes.buttons}>
+                        <Grid container spacing={2} justify="center">
+                            <Grid item>
+                                <Button variant="contained" color="primary">
+                                    See my photos  
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="outlined" color="primary">
+                                    Secondary action
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </div>
+                </Container>
+            </div>
+            <Container className={classes.cardGrid} maxWidth="md">
+                <Grid container spacing={4}>
+                    {cards.map((card) => (
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                        <Card clasName={classes.card}>
+                            <CardMedia 
+                            className={classes.cardMedia}
+                            image="https://source.unsplash.com/random"
+                            title="Image title">
+                            </CardMedia>
+                            <CardContent className={classes.cardContent}>
+                                <Typography gutterBottom variant="h5">
+                                    Heading
+                                </Typography>
+                                <Typography>
+                                    This is a media card. You can use this section to describe the content.
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">View</Button>
+                                <Button size="small" color="primary">Edit</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </main>
+        <footer className={classes.footer}>
+            <Typography variant="h6" align="center" gutterBottom>
+                Footer
+            </Typography>
+            <Typography variant="subtitle1" align="center" color="textSecondary">
+                Something here to give the footer a purpose
+            </Typography>
+        </footer>
+        </>
     )
 }
 
